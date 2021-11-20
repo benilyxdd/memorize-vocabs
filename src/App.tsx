18c43f1f3330cb5db13b8c5dc75ea3f71c96a069
benily <leekwokhinben@gmail.com>
2021-11-20 23:00:50 +0800

    render vocabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import Vocabs from "./constants/Vocabulary.json";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			{Vocabs &&
+				Vocabs.map((vocab) => {
+					return (
+						<div>
+							<h1>{vocab.word}</h1>
+							<h2>{vocab.meaning}</h2>
+							<h2>{vocab.example}</h2>
+						</div>
+					);
+				})}
 		</div>
 	);
 }
