053fcfa896746e89efb2a6100f7b791efa9cb800
benily <leekwokhinben@gmail.com>
2021-11-22 14:05:44 +0800

    add vocabs table skeleton

---
+++ src/components/VocabsTable/VocabsTable.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./VocabsTable.css";
+
+const VocabsTable = () => {
+	return (
+		<div>
+			<h2>This is vocabs table</h2>
+		</div>
+	);
+};
+
+export default VocabsTable;
