323a898fa2024f9169460c3390d2902de51981e1
benily <leekwokhinben@gmail.com>
2021-11-22 14:30:22 +0800

    vocabs table update

--- src/components/VocabsTable/VocabsTable.tsx
+++ src/components/VocabsTable/VocabsTable.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableRow,
+	TableHead,
+	TableContainer,
+	Paper,
+} from "@mui/material";
 
 import "./VocabsTable.css";
+import { VocabularyInterface } from "../../constants/VocabularyInterface";
 
-const VocabsTable = () => {
+type VocabsTabelProps = {
+	vocabsList: Array<VocabularyInterface>;
+};
+
+const VocabsTable = ({ vocabsList }: VocabsTabelProps) => {
 	return (
-		<div>
-			<h2>This is vocabs table</h2>
-		</div>
+		<TableContainer component={Paper}>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell align="right">Phrase / Word</TableCell>
+						<TableCell align="right">Meaning</TableCell>
+						<TableCell align="right">Proficiency</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{vocabsList.map((vocab) => (
+						<TableRow key={vocab.word}>
+							<TableCell align="right">{vocab.word}</TableCell>
+							<TableCell align="right">{vocab.meaning}</TableCell>
+							<TableCell align="right">
+								{vocab.proficiency}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	);
 };
 
