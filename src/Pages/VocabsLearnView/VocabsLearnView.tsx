345ee3e12eae8bb82761fa3ea3a3335103cc09f4
benily <leekwokhinben@gmail.com>
2021-11-22 17:46:52 +0800

    render vocab

--- src/Pages/VocabsLearnView/VocabsLearnView.tsx
+++ src/Pages/VocabsLearnView/VocabsLearnView.tsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import "./VocabsLearnView.css";
+import { RootState } from "../..";
+import { VocabsListInterface } from "../../store/reducers/VocabsData";
+import { VocabularyInterface } from "../../constants/VocabularyInterface";
+
+const getRandomNumber = (
+	upperbound: number,
+	lowerbound: number = 0
+): number => {
+	return Math.floor(Math.random() * (upperbound - lowerbound) + lowerbound);
+};
 
 const VocabsLearnView = () => {
+	const VocabsList = useSelector<RootState, VocabsListInterface>(
+		(state) => state.VocabsData.VocabsList
+	);
+
+	// a list of index -> choose random index in VocabsList
+	const randomList: Array<number> = VocabsList.reduce((acc, vocab, index) => {
+		const newAcc = [...acc];
+		for (let i = 0; i < 5 - vocab.proficiency; i++) {
+			newAcc.push(index);
+		}
+		return newAcc;
+	}, [] as number[]);
+
+	// this will  only be null when initialize
+	const [currentVocab, setCurrentVocab] =
+		useState<VocabularyInterface | null>(null);
+
+	const [answerShowed, setAnswerShowed] = useState<boolean>(false);
+
+	const randomizeVocab = (): void => {
+		const randomNumber = getRandomNumber(VocabsList.length - 1);
+		setCurrentVocab(VocabsList[randomNumber]);
+	};
+
+	useEffect((): void => {
+		randomizeVocab();
+	}, []);
+
 	return (
-		<div>
-			<h1>This is Vocabs Learn View</h1>
-		</div>
+		<>
+			<h1>{currentVocab?.word}</h1>
+			{answerShowed && <h2>{currentVocab?.meaning}</h2>}
+			{answerShowed && (
+				<h2>
+					Example:
+					<br />
+					{currentVocab?.example}
+				</h2>
+			)}
+		</>
 	);
 };
 
