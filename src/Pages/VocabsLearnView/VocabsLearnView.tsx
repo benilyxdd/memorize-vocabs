d2023b5933f90f8ae6ad7a66dcac7a53dd32572f
benily <leekwokhinben@gmail.com>
2021-11-22 14:02:34 +0800

    add vocabs learn page skeleton

---
+++ src/Pages/VocabsLearnView/VocabsLearnView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./VocabsLearnView.css";
+
+const VocabsLearnView = () => {
+	return (
+		<div>
+			<h1>This is Vocabs Learn View</h1>
+		</div>
+	);
+};
+
+export default VocabsLearnView;
