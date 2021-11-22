bc1e000d2003042503880147df2fb8ba3eb89bb8
benily <leekwokhinben@gmail.com>
2021-11-22 22:40:39 +0800

    remove useless thing

--- src/store/reducers/VocabsData.ts
+++ src/store/reducers/VocabsData.ts
@@ -1,6 +1,5 @@
 import Vocabs from '../../constants/Vocabulary.json';
 import { VocabularyInterface } from '../../constants/VocabularyInterface';
-import { addVocabInterface, removeVocabInterface } from '../actions/VocabsData';
 import { ADD_VOCAB, REMOVE_VOCAB } from '../actions/VocabsData';
 
 export type VocabsListInterface = Array<VocabularyInterface>;
