ebbffe4db7b0891d8a64926d47663dbd003b901d
benily <leekwokhinben@gmail.com>
2021-11-22 14:02:43 +0800

    add vocabs list page skeleton

---
+++ src/Pages/VocabsListView/VocabsListView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./VocabsListView.css";
+
+const VocabsListView = () => {
+	return (
+		<div>
+			<h1>This is Vocab list view</h1>
+		</div>
+	);
+};
+
+export default VocabsListView;
