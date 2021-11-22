6e81c3d4e9228f18d6e423ff2f0c1848782857eb
benily <leekwokhinben@gmail.com>
2021-11-22 22:12:47 +0800

    add dialog to list view

--- src/Pages/VocabsListView/VocabsListView.tsx
+++ src/Pages/VocabsListView/VocabsListView.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Button, Stack } from "@mui/material";
 
 import "./VocabsListView.css";
 import VocabsTable from "../../components/VocabsTable/VocabsTable";
 import { RootState } from "../..";
 import { VocabsListInterface } from "../../store/reducers/VocabsData";
+import VocabsListAddDialog from "../../components/VocabsListAddDialog/VocabsListAddDialog";
 
 const VocabsListView = () => {
 	const VocabsList = useSelector<RootState, VocabsListInterface>(
 		(state) => state.VocabsData.VocabsList
 	);
+
+	const [addDialogOpened, setAddDialogOpened] = useState<boolean>(false);
 	return (
 		<>
+			<Stack direction="row" spacing={2} padding={1}>
+				<Button
+					variant="contained"
+					onClick={() => setAddDialogOpened(true)}
+				>
+					Add
+				</Button>
+			</Stack>
+			<VocabsListAddDialog
+				open={addDialogOpened}
+				openf={setAddDialogOpened}
+			/>
 			<VocabsTable vocabsList={VocabsList} />
 		</>
 	);
