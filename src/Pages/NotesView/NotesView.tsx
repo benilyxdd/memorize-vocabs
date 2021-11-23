ad5065baffab9c98cfeaf16d29c1669b0f01ef25
benily <leekwokhinben@gmail.com>
2021-11-23 15:23:05 +0800

    add notes

--- src/Pages/NotesView/NotesView.tsx
+++ src/Pages/NotesView/NotesView.tsx
@@ -5,7 +5,18 @@ import "./NotesView.css";
 const NotesView = () => {
 	return (
 		<div>
-			<h1>this is notes view</h1>
+			<h2>Things to know</h2>
+			<ul>
+				<li>The maximum proficiency is 5 and the minimum is -5</li>
+				<li>
+					Higher proficiency will make a word have a lower chance to
+					appear and vice versa
+				</li>
+				<li>
+					You need to export and save the JSON after adding or
+					removing words
+				</li>
+			</ul>
 		</div>
 	);
 };
